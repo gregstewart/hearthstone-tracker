4cf26d14ac4fdbb1688b9171099cf487573cc5a3
Greg Stewart <gregs@tcias.co.uk>
2016-01-04 17:28:23 +0000

    Create log file

--- index.js
+++ index.js
@@ -1,9 +1,45 @@
+import createFile from 'create-file';
 import app from 'app';
 import BrowserWindow from 'browser-window';
 
 let mainWindow = null;
 
+const createLogFile = () => {
+  const pathToLogFile = process.env['HOME'] + '/Library/Preferences/Blizzard/Hearthstone/log.config';
+  const contents = `[LoadingScreen]
+LogLevel=1
+FilePrinting=false
+ConsolePrinting=true
+ScreenPrinting=false
+
+[Zone]
+LogLevel=1
+FilePrinting=false
+ConsolePrinting=true
+ScreenPrinting=false
+
+[Asset]
+LogLevel=1
+ConsolePrinting=true
+
+[Bob]
+LogLevel=1
+ConsolePrinting=true
+
+[Power]
+LogLevel=1
+ConsolePrinting=true`
+
+  createFile(pathToLogFile, contents, (error) => {
+    if (error) {
+      console.log(error);
+    }
+  });
+}
+
 app.on('ready', () => {
+  createLogFile();
+
   mainWindow = new BrowserWindow({
       height: 600,
       width: 800
