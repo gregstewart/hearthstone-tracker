4ebb7040488e3da447157e4db97d6e46c4d72933
Greg Stewart <gregs@tcias.co.uk>
2016-01-14 00:53:36 +0000

    Add golden path integration test

--- src/log-watcher.js
+++ src/log-watcher.js
@@ -1,12 +1,11 @@
-import LogWatcher from 'hearthstone-log-watcher';
+// import LogWatcher from 'hearthstone-log-watcher';
 
 import { setWinCondition, logMatchData, recordOutcome, resetData, setMatchId, setFor, setAgainst } from './match-data-manipulation';
-import parseFriendlyPlayer from '../app/scripts/parse-friendly-player';
+import {parseFriendlyPlayer} from '../app/scripts/parse-friendly-player';
 import {isMyHero, isHeroCard} from '../app/scripts/is-my-hero';
 import findClass from '../app/scripts/find-class';
 import hasWon from '../app/scripts/win-condition';
 
-let logMatch = [];
 let dataStructure = {
   matchId: "",
   for: "",
@@ -28,23 +27,26 @@ const setHeroValues = (data) => {
   return dataStructure;
 };
 
-const logWatcher = new LogWatcher();
+export function dataLogger (logWatcher) {
+  logWatcher.on('game-start', () => {
+    dataStructure = setMatchId(dataStructure);
+  });
 
-logWatcher.on('game-start', () => {
-  dataStructure = setMatchId(dataStructure);
-});
+  logWatcher.on('game-over', (data) => {
+    dataStructure = setWinCondition(dataStructure, hasWon(parseFriendlyPlayer(data)));
+    database = recordOutcome(database, dataStructure);
+    // let { logMatch, dataStructure } = resetData();
+  });
 
-logWatcher.on('game-over', (data) => {
-  dataStructure = setWinCondition(dataStructure, hasWon(parseFriendlyPlayer(data)));
-  dataStructure = logMatchData(dataStructure, logMatch);
-  database = recordOutcome(database, dataStructure);
-  let { logMatch, dataStructure } = resetData();
-});
+  logWatcher.on('zone-change', (data) => {
+    dataStructure = setHeroValues(data);
+    data.id = Date.now();
+    dataStructure.log.push(data);
+  });
 
-logWatcher.on('zone-change', (data) => {
-  dataStructure = setHeroValues(data);
-  data.id = Date.now();
-  logMatch.push(data);
-});
+  return logWatcher;
+}
 
-logWatcher.start();
+export function getDatabase () {
+  return database;
+}
--- test/integration/log-parser.spec.js
+++ test/integration/log-parser.spec.js
@@ -1,124 +1,163 @@
+import LogWatcher from 'hearthstone-log-watcher';
+import {dataLogger, getDatabase} from '../../src/log-watcher';
+
+import chai from 'chai';
+import sinon from 'sinon';
+import Promise from 'bluebird';
+chai.use(require('sinon-chai'));
+const expect = chai.expect;
 
 describe('Parse HS log file', () => {
+  let logWatcher, sandbox, logData;
+  before(() => {
+    sandbox = sinon.sandbox.create();
+    let watcher = new LogWatcher();
+
+    logWatcher = dataLogger(watcher);
+    sandbox.spy(logWatcher, "on");
+  });
+
+  after(() => {
+    sandbox.restore();
+  });
 
   describe('We have a good parse', () => {
     beforeEach(() => {
-      const log = [
-        { event: 'zone-change'},
-        { cardName: 'Eaglehorn Bow',
+      logData = [
+        { event: 'zone-change',
+        data: { cardName: 'Eaglehorn Bow',
           entityId: 13,
           cardId: 'EX1_536',
           playerId: 1,
           fromTeam: undefined,
           fromZone: undefined,
           toTeam: 'FRIENDLY',
-          toZone: 'HAND' },
-        { event: 'zone-change'},
-        { cardName: 'Dr. Boom',
+          toZone: 'HAND' }},
+        { event: 'zone-change',
+        data: { cardName: 'Dr. Boom',
           entityId: 19,
           cardId: 'GVG_110',
           playerId: 1,
           fromTeam: undefined,
           fromZone: undefined,
           toTeam: 'FRIENDLY',
-          toZone: 'HAND' },
-        { event: 'zone-change'},
-        { cardName: 'Animal Companion',
+          toZone: 'HAND' }},
+        { event: 'zone-change',
+        data: { cardName: 'Animal Companion',
           entityId: 31,
           cardId: 'NEW1_031',
           playerId: 1,
           fromTeam: undefined,
           fromZone: undefined,
           toTeam: 'FRIENDLY',
-          toZone: 'HAND' },
-        { event: 'zone-change'},
-        { cardName: 'Rexxar',
+          toZone: 'HAND' }},
+        { event: 'zone-change',
+        data: { cardName: 'Rexxar',
           entityId: 64,
           cardId: 'HERO_05',
           playerId: 1,
           fromTeam: undefined,
           fromZone: undefined,
           toTeam: 'FRIENDLY',
-          toZone: 'PLAY (Hero)' },
-        { event: 'zone-change'},
-        { cardName: 'Steady Shot',
+          toZone: 'PLAY (Hero)' }},
+        { event: 'zone-change',
+        data: { cardName: 'Steady Shot',
           entityId: 65,
           cardId: 'DS1h_292',
           playerId: 1,
           fromTeam: undefined,
           fromZone: undefined,
           toTeam: 'FRIENDLY',
-          toZone: 'PLAY (Hero Power)' },
-        { event: 'zone-change'},
-        { cardName: 'Thrall',
+          toZone: 'PLAY (Hero Power)' }},
+        { event: 'zone-change',
+        data: { cardName: 'Thrall',
           entityId: 66,
           cardId: 'HERO_02',
           playerId: 2,
           fromTeam: undefined,
           fromZone: undefined,
           toTeam: 'OPPOSING',
-          toZone: 'PLAY (Hero)' },
-        { event: 'game-started'},
-        [ { name: 'zam', id: 2, team: 'OPPOSING' },
-          { name: 'artaios', id: 1, team: 'FRIENDLY' } ],
-        { event: 'zone-change'},
-        { cardName: 'Totemic Call',
+          toZone: 'PLAY (Hero)' }},
+        { event: 'game-start',
+        data: [ { name: 'zam', id: 2, team: 'OPPOSING' },
+          { name: 'artaios', id: 1, team: 'FRIENDLY' } ]},
+        { event: 'zone-change',
+        data: { cardName: 'Totemic Call',
           entityId: 67,
           cardId: 'CS2_049',
           playerId: 2,
           fromTeam: undefined,
           fromZone: undefined,
           toTeam: 'OPPOSING',
-          toZone: 'PLAY (Hero Power)' },
-        { event: 'zone-change'},
-        { cardName: 'Freezing Trap',
+          toZone: 'PLAY (Hero Power)' }},
+        { event: 'zone-change',
+        data: { cardName: 'Freezing Trap',
           entityId: 6,
           cardId: 'EX1_611',
           playerId: 1,
           fromTeam: 'FRIENDLY',
           fromZone: 'DECK',
           toTeam: 'FRIENDLY',
-          toZone: 'HAND' },
-        { event: 'zone-change'},
-        { cardName: 'Dr. Boom',
+          toZone: 'HAND' }},
+        { event: 'zone-change',
+        data: { cardName: 'Dr. Boom',
           entityId: 19,
           cardId: 'GVG_110',
           playerId: 1,
           fromTeam: 'FRIENDLY',
           fromZone: 'HAND',
           toTeam: 'FRIENDLY',
-          toZone: 'DECK' },
-        { event: 'zone-change'},
-        { cardName: 'Webspinner',
+          toZone: 'DECK' }},
+        { event: 'zone-change',
+        data: { cardName: 'Webspinner',
           entityId: 32,
           cardId: 'FP1_011',
           playerId: 1,
           fromTeam: 'FRIENDLY',
           fromZone: 'HAND',
           toTeam: 'FRIENDLY',
-          toZone: 'PLAY' },
-        { event: 'zone-change'},
-        { cardName: 'Webspinner',
+          toZone: 'PLAY' }},
+        { event: 'zone-change',
+        data: { cardName: 'Webspinner',
           entityId: 15,
           cardId: 'FP1_011',
           playerId: 1,
           fromTeam: 'FRIENDLY',
           fromZone: 'DECK',
           toTeam: 'FRIENDLY',
-          toZone: 'HAND' },
-        { event: 'game-ended'},
-        [ { name: 'zam', id: 2, team: 'OPPOSING', status: 'LOST' },
-          { name: 'artaios', id: 1, team: 'FRIENDLY', status: 'WON' }],
-        { event: 'zone-change'},
-        { cardName: 'Thrall',
+          toZone: 'HAND' }},
+        { event: 'game-over',
+        data: [ { name: 'zam', id: 2, team: 'OPPOSING', status: 'LOST' },
+          { name: 'artaios', id: 1, team: 'FRIENDLY', status: 'WON' }]},
+        { event: 'zone-change',
+        data: { cardName: 'Thrall',
           entityId: 66,
           cardId: 'HERO_02',
           playerId: 2,
           fromTeam: 'OPPOSING',
           fromZone: 'PLAY (Hero)',
           toTeam: 'OPPOSING',
-          toZone: 'GRAVEYARD' }
+          toZone: 'GRAVEYARD' }}
       ];
+    });
+
+    it('returns a database with a complete record for the match', () => {
+      const applyData = () => {
+        return new Promise((resolve) => {
+          logData.map((element) => {
+            logWatcher.emit(element.event, element.data);
+          });
+          return resolve();
+        });
+      };
+
+      applyData().then(() => {
+        expect(getDatabase()[0].matchId).to.be.a.number;
+        expect(getDatabase()[0].for).to.equal('Hunter');
+        expect(getDatabase()[0].against).to.equal('Shaman');
+        expect(getDatabase()[0].log.length).to.equal(logData.length-3);
+        expect(getDatabase()[0].hasWon).to.be.true;
+      });
     });
   });
 
