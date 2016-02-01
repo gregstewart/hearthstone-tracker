3b48759014e96921e72ce0f1cdc9688a9d342a63
Greg Stewart <gregs@tcias.co.uk>
2016-02-01 16:34:55 +0000

    Configure redux store

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,18 +1,26 @@
 import mori from 'mori';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
 import Main from '../views/main.jsx';
+import stats from './reducers/stats';
 import {ipcRenderer} from 'electron';
+import {updateStats} from './actions/stats';
 
 let data = [
-  {id: 1, label: "Wins", text: "10"},
-  {id: 2, label: "Losses", text: "5"},
-  {id: 3, label: "Ratio", text: "66%"}
+  {id: 1, label: "Wins", text: "0"},
+  {id: 2, label: "Losses", text: "0"},
+  {id: 3, label: "Ratio", text: "0%"}
 ];
 
-ipcRenderer.on('ping', function(event, message) {
-  data = message;  // logs message
+let store = createStore(stats, data);
+
+ipcRenderer.on('ping', function (event, message) {
+  /*eslint-disable no-console */
   console.log(message);  // logs data
+  store.dispatch(updateStats(message));
 });
 
 ReactDOM.render(<Main data={data} />, document.getElementById('main'));
+
+window.store = store;
