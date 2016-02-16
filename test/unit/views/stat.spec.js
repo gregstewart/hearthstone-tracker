e8f7e46fff066713b79442e78ad6f041f5f0e0a4
Greg Stewart <gregs@tcias.co.uk>
2016-02-16 21:44:38 +0000

    Add Stat component test

---
+++ test/unit/views/stat.spec.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { createRenderer } from 'react-addons-test-utils';
+import { expect } from 'chai';
+
+import Stat from '../../../app/views/stat.jsx';
+
+describe('Stat item view component', () => {
+  it('renders the component with the correct props', () => {
+    const statData = {id: 1, label: "Wins", text: "3"};
+    const shallowRenderer = createRenderer();
+    shallowRenderer.render(
+      <Stat element={statData} key={statData.id}/>
+    );
+    const item = shallowRenderer.getRenderOutput();
+
+    expect(item.type).to.equal('li');
+    expect(item.props.children).to.deep.equal([ 'Wins', ': ', '3' ]);
+  });
+});
