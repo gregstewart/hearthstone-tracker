feb5175a07c352dc9617539f1fb2976ba7389027
Greg Stewart <gregs@tcias.co.uk>
2016-02-22 22:53:43 +0000

    Refactor Main component

--- app/views/main.jsx
+++ app/views/main.jsx
@@ -1,20 +1,18 @@
 import { summaryStats, winStreak } from '../scripts/validators';
-import React, { Component } from 'react';
+import React from 'react';
 import Stats from './stats';
 import WinStreaks from './win-streaks';
 
-class Main extends Component {
-  render () {
-    return (
-      <div>
-        <h1>Stats</h1>
-        <Stats data={this.props.summaryStats} />
-        <h1>Win Streak</h1>
-        <WinStreaks data={this.props.winStreak} />
-      </div>
-    );
-  }
-}
+const Main = ({
+  summaryStats, winStreak
+}) => (
+  <div>
+    <h1>Stats</h1>
+    <Stats data={summaryStats} />
+    <h1>Win Streak</h1>
+    <WinStreaks data={winStreak} />
+  </div>
+);
 
 Main.propTypes = {
   summaryStats,
--- test/unit/views/main.spec.js
+++ test/unit/views/main.spec.js
@@ -4,9 +4,9 @@ import { expect } from 'chai';
 
 import Main from '../../../app/views/main.jsx';
 
-describe.only('Main view component', () => {
+describe('Main view component', () => {
   it('renders the component with the correct props', () => {
-    const summaryStatData = [{id: 1, label: "Wins", text: "3"}, {id: 2, label: "Loss", text: "1"}];
+    const summaryStatData = [{id: 1, label: 'Wins', text: '3'}, {id: 2, label: 'Loss', text: '1'}];
     const winStreakData = [
       { result: 'loss', as: 'Rogue', against: 'Rogue' },
       { result: 'win', as: 'Rogue', against: 'Mage' }
@@ -16,7 +16,7 @@ describe.only('Main view component', () => {
       <Main summaryStats={summaryStatData} winStreak={winStreakData}/>
     );
     const item = shallowRenderer.getRenderOutput();
-    
+
     expect(item.type).to.equal('div');
     expect(item.props.children[0].type).to.equal('h1');
     expect(item.props.children[0].props.children).to.equal('Stats');
