5425b33db88264625a1fa11f4e4a47ce24b58e08
Greg Stewart <gregs@tcias.co.uk>
2016-06-05 22:43:29 +0100

    Feature flip datascript summary generation

--- src/ui-data/generate-summary.js
+++ src/ui-data/generate-summary.js
@@ -1,17 +1,31 @@
 import { summaryStats, gameBreakdownDetails } from './stats';
+import { summaryStats as summaryStatsDS } from '../datascript/stats';
 import winston from 'winston';
 import winStreak from '../win-streak';
-import { mori } from 'datascript-mori';
+import { mori, datascript } from 'datascript-mori';
+import importer from '../datascript/import';
+import { scheme } from '../datascript/scheme';
 
-const { map, toClj, updateIn, vector } = mori;
+const { map, parse, toClj, updateIn, vector } = mori;
+const { core } = datascript;
 
 const stripLog = (row) => {
   return updateIn(row, ['doc', 'log'], () => { return vector(); });
 };
 
-export function generateSummary (result, wC) {
-  const reducedSet = map(stripLog, toClj(result.rows));
-  return Promise.all([summaryStats(reducedSet), winStreak(reducedSet), gameBreakdownDetails(reducedSet)])
+export function generateSummary (result, wC, flipit) {
+  let reducedSet, promises;
+  if(flipit && flipit.isEnabled('dataScript')) {
+    const db = core.empty_db(scheme);
+    const dbWithData = importer(db, result);
+
+    promises = [summaryStatsDS(dbWithData), winStreak(reducedSet), gameBreakdownDetails(reducedSet)];
+  } else {
+    reducedSet = map(stripLog, toClj(result.rows));
+    promises = [summaryStats(reducedSet), winStreak(reducedSet), gameBreakdownDetails(reducedSet)];
+  }
+
+  return Promise.all(promises)
     .then((results) => {
       wC.send('ping', {summaryStats: results[0], winStreak: results[1], matchBreakdown: results[2]});
     })
