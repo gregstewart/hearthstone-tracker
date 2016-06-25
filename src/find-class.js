5a044e40ff9603fea26e9aec609feb615545adc7
Greg Stewart <gregs@tcias.co.uk>
2016-06-25 01:24:43 +0100

    Added training heros

--- src/find-class.js
+++ src/find-class.js
@@ -12,7 +12,13 @@ const CLASSES = {
   "Anduin Wrynn": "Priest",
   "Valeera Sanguinar": "Rogue",
   "Gul'dan": "Warlock",
-  "Malfurion Stormrage": "Druid"
+  "Malfurion Stormrage": "Druid",
+  "Hogger": "Training",
+  "Millhouse Manastorm": "Training",
+  "Lorewalker Cho": "Training",
+  "King Mukla": "Training",
+  "Hemet Nesingwary": "Training",
+  "Illidan Stormrage": "Training"
 };
 
 export default function (name) {
