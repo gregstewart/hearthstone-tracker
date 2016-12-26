08c9a064e9155f826e1cc8373d3beb0e3f2b9523
Greg Stewart <gregs@tcias.co.uk>
2016-12-26 22:23:30 +0100

    Use the data dump

--- test/unit/datascript/stats.spec.js
+++ test/unit/datascript/stats.spec.js
@@ -1,3 +1,5 @@
+import fs from 'fs';
+
 import { result } from '../../fixtures/database-result';
 import { mori, datascript } from 'datascript-mori';
 const { core } = datascript;
@@ -8,9 +10,10 @@ import { scheme } from '../../../src/datascript/scheme';
 
 import { pluckStats, summaryStats, aggregateDetails, aggregate, gameBreakdownDetails } from '../../../src/datascript/stats';
 
-describe('UI Data - Datascript', () => {
+describe.only('UI Data - Datascript', () => {
   let db, dbWithData;
   beforeEach(() => {
+    const result = JSON.parse(fs.readFileSync('./test/fixtures/data.json', 'utf-8'));
     db = core.empty_db(scheme);
     dbWithData = importer(db, result);
   });
@@ -32,9 +35,9 @@ describe('UI Data - Datascript', () => {
   it('returns win loss summary as a mori hashmap', () => {
     let stats = pluckStats(dbWithData);
 
-    expect(stats.wins).to.equal(5);
-    expect(stats.losses).to.equal(2);
-    expect(stats.ratio).to.equal('71.42857142857143%');
+    expect(stats.wins).to.equal(157);
+    expect(stats.losses).to.equal(139);
+    expect(stats.ratio).to.equal('53.04054054054054%');
   });
 
   it('returns an error when no result is passed in', () => {
@@ -70,11 +73,11 @@ describe('UI Data - Datascript', () => {
     let stats = toJs(aggregate(dbWithData));
 
     expect(stats[0].status).to.equal('wins');
-    expect(stats[0].outcomes[0]).to.deep.equal({'class': 'Rogue', 'total': 3, 'percentage': '60%'});
-    expect(stats[0].outcomes[1]).to.deep.equal({'class': 'Priest', 'total': 1, 'percentage': '20%'});
+    expect(stats[0].outcomes[0]).to.deep.equal({'class': 'Warrior', 'total': 58, 'percentage': '36.94267515923567%'});
+    expect(stats[0].outcomes[1]).to.deep.equal({'class': 'Paladin', 'total': 39, 'percentage': '24.840764331210192%'});
     expect(stats[1].status).to.equal('losses');
-    expect(stats[1].outcomes[0]).to.deep.equal({'class': 'Rogue', 'total': 1, 'percentage': '50%'});
-    expect(stats[1].outcomes[1]).to.deep.equal({'class': 'Warlock', 'total': 1, 'percentage': '50%'});
+    expect(stats[1].outcomes[0]).to.deep.equal({'class': 'Mage', 'total': 26, 'percentage': '18.705035971223023%'});
+    expect(stats[1].outcomes[1]).to.deep.equal({'class': 'Warlock', 'total': 18, 'percentage': '12.949640287769784%'});
   });
 
   describe('takes a filtered mori result', () => {
@@ -82,27 +85,27 @@ describe('UI Data - Datascript', () => {
       it('returns the expected result as a mori hashmap', () => {
         let stats = aggregateDetails(dbWithData, true);
 
-        expect(get(nth(stats, 0), 'class')).to.equal('Rogue');
-        expect(get(nth(stats, 0), 'total')).to.equal(3);
-        expect(get(nth(stats, 0), 'percentage')).to.equal('60%');
-        expect(get(nth(stats, 1), 'class')).to.equal('Priest');
-        expect(get(nth(stats, 1), 'total')).to.equal(1);
-        expect(get(nth(stats, 1), 'percentage')).to.equal('20%');
-        expect(get(nth(stats, 2), 'class')).to.equal('Druid');
-        expect(get(nth(stats, 2), 'total')).to.equal(1);
-        expect(get(nth(stats, 2), 'percentage')).to.equal('20%');
+        expect(get(nth(stats, 0), 'class')).to.equal('Priest');
+        expect(get(nth(stats, 0), 'total')).to.equal(17);
+        expect(get(nth(stats, 0), 'percentage')).to.equal('10.828025477707007%');
+        expect(get(nth(stats, 1), 'class')).to.equal('Paladin');
+        expect(get(nth(stats, 1), 'total')).to.equal(39);
+        expect(get(nth(stats, 1), 'percentage')).to.equal('24.840764331210192%');
+        expect(get(nth(stats, 2), 'class')).to.equal('Warrior');
+        expect(get(nth(stats, 2), 'total')).to.equal(58);
+        expect(get(nth(stats, 2), 'percentage')).to.equal('36.94267515923567%');
       });
     });
     describe('losses grouped by class', () => {
       it('returns the expected result as a mori hashmap', () => {
         let stats = aggregateDetails(dbWithData, false);
 
-        expect(get(nth(stats, 0), 'class')).to.equal('Rogue');
-        expect(get(nth(stats, 0), 'total')).to.equal(1);
-        expect(get(nth(stats, 0), 'percentage')).to.equal('50%');
-        expect(get(nth(stats, 1), 'class')).to.equal('Warlock');
-        expect(get(nth(stats, 1), 'total')).to.equal(1);
-        expect(get(nth(stats, 1), 'percentage')).to.equal('50%');
+        expect(get(nth(stats, 0), 'class')).to.equal('Paladin');
+        expect(get(nth(stats, 0), 'total')).to.equal(12);
+        expect(get(nth(stats, 0), 'percentage')).to.equal('8.633093525179856%');
+        expect(get(nth(stats, 1), 'class')).to.equal('Warrior');
+        expect(get(nth(stats, 1), 'total')).to.equal(14);
+        expect(get(nth(stats, 1), 'percentage')).to.equal('10.071942446043165%');
       });
     });
   });
