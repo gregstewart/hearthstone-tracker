9c7a391f62a00a0de4085aa9ed6dfbabbc7f2444
Greg Stewart <gregs@tcias.co.uk>
2016-12-27 20:17:06 +0100

    Apply a little bit of styling

--- app/views/main.jsx
+++ app/views/main.jsx
@@ -4,28 +4,37 @@ import Stats from './stats.jsx';
 import WinStreaks from './win-streaks.jsx';
 import MatchBreakdown from './match-breakdown.jsx';
 
+const divStyle = {
+  float: 'left',
+  margin: '0 20px 0 0'
+};
+
+const clearStyle = {
+  clear: 'both'
+}
+
 const Main = ({
   matchBreakdown, summaryStats, winStreak
 }) => (
   <div>
     <h1>Stats</h1>
-    <div>
+    <div style={divStyle}>
       <h2>Today</h2>
       <Stats data={summaryStats.today} />
     </div>
-    <div>
+    <div style={divStyle}>
       <h2>This week</h2>
       <Stats data={summaryStats.thisWeek} />
     </div>
-    <div>
+    <div style={divStyle}>
       <h2>This month</h2>
       <Stats data={summaryStats.thisMonth} />
     </div>
-    <div>
+    <div style={divStyle}>
       <h2>All time</h2>
       <Stats data={summaryStats.allTime} />
     </div>
-    <h1>Win Streak</h1>
+    <h1 style={clearStyle}>Win Streak</h1>
     <WinStreaks data={winStreak} />
     <h1>Breakdown of matches</h1>
     <MatchBreakdown data={matchBreakdown} />
